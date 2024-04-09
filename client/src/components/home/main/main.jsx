25aaeeae990e8f6d03230fe4d74935e91e6c65ef
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2024-04-09 11:56:59 +0500

    all-information

--- client/src/components/home/main/main.jsx
+++ client/src/components/home/main/main.jsx
@@ -6,8 +6,8 @@ const Main = () => {
   const { darkMode} = useTheme();
   return (
     <div className={ darkMode 
-        ? "col-span-5 h-screen bg-slate-700 p-8" 
-        : "col-span-5 h-screen bg-neutral-100 p-8"}
+        ? "col-span-5 h-[100vh] overflow-auto bg-slate-700 p-8 " 
+        : "col-span-5 h-[100vh] overflow-auto bg-neutral-100 p-8 "}
     >
       <Routes>
         <Route path='/all-information' element={<AllInformation/>}/>
--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -2,6 +2,10 @@ import { SiHomeassistant } from "react-icons/si";
 import { MdOutlineGroupAdd } from "react-icons/md";
 import { RxUpdate } from "react-icons/rx";
 import { FaUserCog } from "react-icons/fa";
+import { TbBrandVscode } from "react-icons/tb";
+import { LiaLaptopCodeSolid } from "react-icons/lia";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { PiStudentBold } from "react-icons/pi";
 
 export const dataMenu = [
   {
@@ -24,4 +28,95 @@ export const dataMenu = [
     title: "My Account",
     link: '/my-account',
   },
-]
+];
+
+export const cards = [
+  {
+    title: "Mentors",
+    icon: FaChalkboardTeacher,
+  },
+  {
+    title: "Pupils",
+    icon: PiStudentBold,
+  },
+];
+
+export const categories = [
+  {
+    title:"Web-dasturlash",
+    icon: TbBrandVscode,
+  },
+  {
+    title:"IT kids",
+    icon: LiaLaptopCodeSolid,
+  },
+  {
+    title:"Kiber havfsizlik",
+    icon: TbBrandVscode,
+  },
+  {
+    title:"Ingliz tili",
+    icon: TbBrandVscode,
+  },
+  {
+    title:"Rus tili",
+    icon: TbBrandVscode,
+  },
+  {
+    title:"Matematika",
+    icon: TbBrandVscode,
+  },
+];
+
+export const mentors = [
+  {
+    username:"Abdushoxid Bannayev",
+    phoneNumber:"+99890 122 0000",
+    createdAt:"04/09/2024",
+  },
+  {
+    username:"Ayubxon Ahmatxonov",
+    phoneNumber:"+99891 555 5555",
+    createdAt:"04/09/2024",
+  },
+  {
+    username:"Abdushoxid Bannayev",
+    phoneNumber:"+99890 122 0000",
+    createdAt:"04/09/2024",
+  },
+  {
+    username:"Ayubxon Ahmatxonov",
+    phoneNumber:"+99891 555 5555",
+    createdAt:"04/09/2024",
+  },
+  {
+    username:"Abdushoxid Bannayev",
+    phoneNumber:"+99890 122 0000",
+    createdAt:"04/09/2024",
+  },
+  {
+    username:"Ayubxon Ahmatxonov",
+    phoneNumber:"+99891 555 5555",
+    createdAt:"04/09/2024",
+  },
+  {
+    username:"Ibrohimjon Jakbaraliyev",
+    phoneNumber:"+99897 777 7777",
+    createdAt:"04/09/2024",
+  },
+  {
+    username:"Abdushoxid Bannayev",
+    phoneNumber:"+99890 122 0000",
+    createdAt:"04/09/2024",
+  },
+  {
+    username:"Ayubxon Ahmatxonov",
+    phoneNumber:"+99891 555 5555",
+    createdAt:"04/09/2024",
+  },
+  {
+    username:"Ibrohimjon Jakbaraliyev",
+    phoneNumber:"+99897 777 7777",
+    createdAt:"04/09/2024",
+  },
+];
